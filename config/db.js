20663658879db49f5a205909038c8f831ab1c7d4
Aksh123456 <aksh.211999@gmail.com>
2024-11-30 11:15:59 +0530

    change db url

--- config/db.js
+++ config/db.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/auction');
+    // await mongoose.connect('mongodb://127.0.0.1:27017/auction');
+    await mongoose.connect('mongodb+srv://devakshay2107:62YhdkBb4z8VkyPv@realtimeauctionsystem.1u0ic.mongodb.net/RTAS?retryWrites=true&w=majority').then(()=> {
+      console.log('db')
+    }).catch(err =>{
+      console.log(err, 'err')
+    });
+
+    // mongodb+srv://devakshay2107:62YhdkBb4z8VkyPv@realtimeauctionsystem.1u0ic.mongodb.net/?retryWrites=true&w=majority&appName=RealTimeAuctionSystem
+
+    // mongodb+srv://devakshay2107:<db_password>@realtimeauctionsystem.1u0ic.mongodb.net/?retryWrites=true&w=majority&appName=RealTimeAuctionSystem
     console.log("Database connected successfully");
   } catch (error) {
     console.error("Database connection failed:", error);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose')
 
 exports.register = async (req, res) => {
   const { username, email, password } = req.body;
@@ -12,7 +13,11 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  console.log(req.body, 'body')
+  // mongoose.set("debug", true);
+
+  const user = await User.findOne({ email: email });
+  console.log(user, 'user')
   if (!user || !(await bcrypt.compare(password, user.password)))
     return res.status(401).json({ message: "Invalid credentials" });
   const token = jwt.sign({ id: user._id, email: user.email }, "secret", { expiresIn: "1h" });
